aa5edeada1c1bb0becff3fc5c4442563a5b811ff
Luka Bezovšek <luka.bezovsek.mmv@gmail.com>
2019-04-03 00:37:30 +0200

    Fixed export function, specify domain for which you want to fetch links

--- export.py
+++ export.py
@@ -1,7 +1,14 @@
 import json
 from database_handler import DatabaseHandler
 
-# EXPORT DATA FOR PACK VISUALISATION SITES AND PAGES
+"""
+    Run this script every time you want to export data from db for visualisation. For every graphic json file is
+    generated and placed into prepared directory (visualisation/...)
+"""
+
+
+# ----------- EXPORT DATA FOR PACK VISUALISATION SITES AND PAGES -------------------
+
 export_data = {
     'name': 'Sites',
     'children': []
@@ -34,19 +41,16 @@ for site in sites:
         }
     )
 
-
-#print("EXPORT DATA: ", export_data)
-
-with open('data.json', 'w') as outfile:
+with open('visualisation/pages/data.json', 'w') as outfile:
     json.dump(export_data, outfile)
 
-# EXPORT DATA FOR NETWORK VISUALISATION CONNECTIONS BETWEEN PAGES
+# ----------- EXPORT DATA FOR NETWORK VISUALISATION CONNECTIONS BETWEEN PAGES ---------------------
 
 export_links_data = {
     'links': []
 }
 
-links = database_handler.fetch_links_from_specific_site()
+links = database_handler.fetch_links_from_specific_domain('http://evem.gov.si/')
 
 for link in links:
     export_links_data['links'].append(
@@ -54,11 +58,9 @@ for link in links:
             'source': link[2],
             'target': link[3],
             'weight': 1,
-            'type': 'licencing'
+            'type': ''
         }
     )
 
-print("EXPORT LINKS: ", export_links_data)
-
-with open('links_data.json', 'w') as links_outfile:
+with open('visualisation/links/data.json', 'w') as links_outfile:
     json.dump(export_links_data, links_outfile)
--- visualisation/pages/pages.js
+++ visualisation/pages/pages.js
@@ -12,7 +12,7 @@ let pack = d3.pack()
 	.size([diameter - margin, diameter - margin])
 	.padding(2);
 
-d3.json("data_3000.json", function(error, root) {
+d3.json("data.json", function(error, root) {
 	if (error) throw error;
 
 	root = d3.hierarchy(root)
